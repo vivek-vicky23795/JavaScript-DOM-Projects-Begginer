bf5164612e149a591f65c8fcded3380e31ba306c
VIVEK MH <69760385+vivek-vicky23795@users.noreply.github.com>
2021-08-25 08:20:54 +0530

    Add files via upload

---
+++ Turn On and Off Bulb/app.js
@@ -0,0 +1,18 @@
+// button-1 : Off  function 
+
+function a() {
+
+    document.getElementById("img1").src="img/img1.png"; 
+
+} 
+
+// function 2 for bulb on 
+
+
+function b() {
+
+    document.getElementById("img1").src="img/img-2.png"; 
+
+}
+
+
